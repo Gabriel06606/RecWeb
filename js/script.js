bb76c0b33c30aa9e399b8c550ee5ab897331b618
Gabriel06606 <ga2_sager@hotmail.com>
2020-12-03 21:38:03 -0300

    Primeiro Commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+// function saveUn() {
+//     var test = [un, un2];
+//   var username = document.getElementById('un').value;
+//   var username = document.getElementById('un2').value;
+//   alert(username);
+// }
+
+$(document).ready(function(){
+    $("#bAdd").click(function(){
+        alert("foda-se");
+
+    });
+});
