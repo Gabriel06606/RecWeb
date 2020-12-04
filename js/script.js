28400bdaf47e18fc6c30ff7f63d40c646faf480a
Gabriel06606 <71332923+Gabriel06606@users.noreply.github.com>
2020-12-03 21:57:56 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+function save() {
+    
+  var username = document.getElementById('nome').value; 
+  var username = document.getElementById('sobrenome').value;
+  var username = [nome, sobrenome];
+  alert(username);
+}
